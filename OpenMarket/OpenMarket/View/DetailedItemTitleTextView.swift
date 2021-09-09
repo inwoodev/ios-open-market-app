5f423b264bfe17145b80445e63b10f9eb248326f
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-09-09 18:40:35 +0900

    Feat : 커스텀 텍스트뷰 구현

---
+++ OpenMarket/OpenMarket/View/DetailedItemTitleTextView.swift
@@ -0,0 +1,45 @@
+//
+//  DetailedItemTitleTextView.swift
+//  OpenMarket
+//
+//  Created by James on 2021/09/06.
+//
+
+import UIKit
+
+final class DetailedItemTitleTextView: UITextView {
+    
+    weak var titleViewDelegate: PatchingTextConvertible?
+    
+    private let defaultMessage: String = "상품 이름"
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        self.adjustsFontForContentSizeCategory = true
+        self.font = UIFont.preferredFont(forTextStyle: .title3)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.autocapitalizationType = .none
+        self.autocorrectionType = .no
+        self.isUserInteractionEnabled = false
+        self.delegate = self
+        self.isScrollEnabled = false
+    }
+    
+    required init?(coder: NSCoder) {
+        super .init(coder: coder)
+    }
+}
+
+extension DetailedItemTitleTextView: UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = defaultMessage
+            textView.textColor = .lightGray
+        }
+        
+        guard textView.text != defaultMessage,
+              textView.text.isEmpty else { return }
+        
+        titleViewDelegate?.convertOptionalTextToDictionary(OpenMarketItemToPostOrPatch.title, textView.text)
+    }
+}
