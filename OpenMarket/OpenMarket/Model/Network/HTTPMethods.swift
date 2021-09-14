63bcec57e5d5340458758ada0a6e3528e8402311
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-09-14 14:59:35 +0900

    Refactor : HTTPMethods 수정
    rawValue 대신 description을 통해 문자열 값에 접근하도록 수정

--- OpenMarket/OpenMarket/Model/Network/HTTPMethods.swift
+++ OpenMarket/OpenMarket/Model/Network/HTTPMethods.swift
@@ -7,9 +7,22 @@
 
 import Foundation
 
-public enum HTTPMethods: String {
-    case get = "GET"
-    case post = "POST"
-    case patch = "PATCH"
-    case delete = "DELETE"
+public enum HTTPMethods {
+    case get
+    case post
+    case patch
+    case delete
+    
+    var description: String {
+        switch self {
+        case .get:
+            return "GET"
+        case .post:
+            return "POST"
+        case .patch:
+            return "PATCH"
+        case .delete:
+            return "DELETE"
+        }
+    }
 }
